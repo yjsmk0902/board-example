d8f83d9ff2d98b5dedf4694894149c0e6318269c
Luke_YS <yjsmk0902@gmail.com>
2023-02-19 17:10:22 +0900

    Member 도메인 수정

--- src/main/java/practice/board/domain/entity/Member.java
+++ src/main/java/practice/board/domain/entity/Member.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.util.ArrayList;
 
 @Entity
@@ -18,13 +19,15 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+    @NotBlank(message = "아이디를 입력해주세요.")
     private String username;
 
+    @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
 
     private String email;
 
-    //private List<Board> boardList = new ArrayList<>();
+//    private List<Board> boardList = new ArrayList<>();
 
     @Builder
     public Member(String username, String password, String email) {
